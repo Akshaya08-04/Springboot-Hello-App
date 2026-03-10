6d0f8a4f2f1df0012f6aaec5777b09162d6895f2
akspriya200408@gmail.com <akspriya200408@gmail.com>
2026-03-10 15:14:33 +0530

    [Akshaya Priya]:Added:UC2:Hello Message API using query parameter

--- src/main/java/com/bridgelabz/helloapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/helloapp/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.bridgelabz.helloapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+//uc1
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from BridgeLabz";
+        return "Hello from Akshaya";
+    }
+    //uc2
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from Akshaya";
     }
 
 }
